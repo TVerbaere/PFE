cb6bb5e20d59e3dede92955023d25eb769919f65
Thibaud <thibaud.verbaere@gmail.com>
2016-01-03 18:07:35 +0100

    fix(BlueToothFacade): fix bug for BlueToothFacade rename BluetoothFacade to BlueToothFacade

--- deactivationservice/src/main/java/iagl/pfe/deactivation/facades/BlueToothFacade.java
+++ deactivationservice/src/main/java/iagl/pfe/deactivation/facades/BlueToothFacade.java
@@ -4,7 +4,7 @@ package iagl.pfe.deactivation.facades;
  * Interface of the Facade for BlueTooth
  * @author T. VERBAERE
  */
-public interface BluetoothFacade {
+public interface BlueToothFacade {
 
     /**
      * Tests if the bluetooth is activated.
--- deactivationservice/src/main/java/iagl/pfe/deactivation/facades/implementation/BlueToothFacadeImpl.java
+++ deactivationservice/src/main/java/iagl/pfe/deactivation/facades/implementation/BlueToothFacadeImpl.java
@@ -2,14 +2,14 @@ package iagl.pfe.deactivation.facades.implementation;
 
 import android.bluetooth.BluetoothAdapter;
 
-import iagl.pfe.deactivation.facades.BluetoothFacade;
+import iagl.pfe.deactivation.facades.BlueToothFacade;
 import iagl.pfe.deactivation.facades.Facade;
 
 /**
  * Facade for BlueThooth
  * @author T. VERBAERE
  */
-public class BlueToothFacadeImpl implements BluetoothFacade, Facade {
+public class BlueToothFacadeImpl implements BlueToothFacade, Facade {
 
     /**
      * Creates a new BlueTooth facade.
